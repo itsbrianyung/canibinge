84f2d22fd1c8f76db4a52674faef169c2d6147f9
itsbrianyung <62833558+itsbrianyung@users.noreply.github.com>
2020-03-29 11:14:53 -0700

    Update themoviedb.js

--- js/themoviedb.js
+++ js/themoviedb.js
@@ -11,8 +11,8 @@ var theMovieDb = {};
 
 theMovieDb.common = {
   api_key: "54e8661cf9f6d1d987d2cc01a5a1e2d8",
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "en-US",
   generateQuery: function(options) {
